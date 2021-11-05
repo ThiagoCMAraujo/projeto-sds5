056425e0efd702e1c207b9c06e965e7ef5bfcc79
tmelo387 <tmelo387@gmail.com>
2021-11-05 11:00:45 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "assets/components/dataTable/dataTable";
+import Footer from "assets/components/footer";
+import NavBar from "assets/components/navbar";
 
 function App() {
   return (
-    <div>
+    <>
+      <NavBar/>
+    <div className="container">
       <h1 className="text-primary" >"Olá, mundo!"</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
