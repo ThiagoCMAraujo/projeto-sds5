a7137bfdbfcb5af6e8df0531282e0020b9a8c3c8
tmelo387 <tmelo387@gmail.com>
2021-11-06 18:19:27 -0300

    "Home, navigation"

--- frontend/src/Rotas.tsx
+++ frontend/src/Rotas.tsx
@@ -1,13 +1,14 @@
 import Home from 'pages/Home';
 import Dashboard from 'pages/Dashboard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const Rotas = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>} />
                     
-                <Route path="assets/pages/Dashboard" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
                  
             </Routes>
         </BrowserRouter>
